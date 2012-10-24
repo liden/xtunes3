5d8476c020fe95f191f92193972dbd6a5a17fb56
Lara Iden <lara.iden@gmail.com>
2012-10-23 23:07:56 -0400

    initial commit xtunes3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xtunes3::Application.config.secret_token = 'ec11ea132c7061d6a6e07e1983aa777fa7fb095fa95b8b03b210b63b554bf99f4993829e6c4c4fccf2b76f626a6c0a39ab0515472b2afa29891c040839122bce'
