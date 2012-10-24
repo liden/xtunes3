bbc71c86bbd29993ca2e09c3a093b8579ac1b132
Lara Iden <lara.iden@gmail.com>
2012-10-24 11:05:25 -0400

    create album model + controller + views

---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,45 @@
+class AlbumsController < ApplicationController
+  def index
+    @albums = Album.all
+  end
+
+  def show
+    @album = Album.find(params[:id])
+  end
+
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(params[:album])
+
+    if @album.save
+      redirect_to albums_path, notice: 'album was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @album = Album.find(params[:id])
+  end
+
+  def update
+    @album = Album.find(params[:id])
+
+    if @album.update_attributes(params[:album])
+      redirect_to albums_path, notice: 'album was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @album = Album.find(params[:id])
+    @album.destroy
+
+    redirect_to albums_path
+  end  
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121024140118) do
+ActiveRecord::Schema.define(:version => 20121024144323) do
+
+  create_table "albums", :force => true do |t|
+    t.string   "name"
+    t.integer  "release_year"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "artists", :force => true do |t|
     t.string   "name"
