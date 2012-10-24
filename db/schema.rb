daefbcbd34310443e3a890424b5d054fdd609c3c
Lara Iden <lara.iden@gmail.com>
2012-10-23 23:54:18 -0400

    add carrierwave + audiofile migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121024032620) do
+ActiveRecord::Schema.define(:version => 20121024034942) do
 
   create_table "posts", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20121024032620) do
     t.decimal  "price"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "audiofile"
   end
 
 end
