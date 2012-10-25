9bbcbe97616dc58e5e9069565bc5a3efd3bb5de8
Lara Iden <lara.iden@gmail.com>
2012-10-25 09:56:04 -0400

    Re-creating artists_songs table migration - old file had a typo so never ran successfully

--- db/migrate/20121025135345_add_artists_songs_join_table.rb
+++ db/migrate/20121025135345_add_artists_songs_join_table.rb
@@ -1,7 +1,8 @@
 class AddArtistsSongsJoinTable < ActiveRecord::Migration
   def change
-  	create_table :artists_songs, :id => fals do |t|
-  		t.integer :artist_id
+    create_table :artists_songs, :id => false do |t|
+      t.integer :artist_id
       t.integer :song_id
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121025134357) do
+ActiveRecord::Schema.define(:version => 20121025135345) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
@@ -28,6 +28,11 @@ ActiveRecord::Schema.define(:version => 20121025134357) do
     t.string   "photo"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "artists_songs", :id => false, :force => true do |t|
+    t.integer "artist_id"
+    t.integer "song_id"
   end
 
   create_table "genres", :force => true do |t|
