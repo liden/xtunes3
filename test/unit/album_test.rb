bafb1a8cfa6e84e979f8ad6eb8df6f7ea5c69f13
Lara Iden <lara.iden@gmail.com>
2012-10-25 21:11:52 -0400

    add test files from model changes

--- test/unit/album_test.rb
+++ test/unit/album_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: albums
+#
+#  id           :integer          not null, primary key
+#  name         :string(255)
+#  release_year :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  genre_id     :integer
+#
+
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
--- test/unit/purchase_test.rb
+++ test/unit/purchase_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: purchases
+#
+#  id         :integer          not null, primary key
+#  price_paid :decimal(, )
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#  song_id    :integer
+#
+
 require 'test_helper'
 
 class PurchaseTest < ActiveSupport::TestCase
