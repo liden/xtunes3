8f910ee45bbe6078224a21ab2e40b4d67a91369b
Lara Iden <lara.iden@gmail.com>
2012-10-25 21:09:55 -0400

    add fields to user registration form + updated controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,9 +1,17 @@
 class AccountsController < ApplicationController
 
   def register
+    @user = User.new
   end
 
   def create
+    user = User.new(params[:user])
+
+    if user.save
+      redirect_to home_path
+    else
+      render :register
+    end
   end
 
 end
