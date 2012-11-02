a9ff36e3adb0d130d187904428e6da7aa44fc19d
Lara Iden <lara.iden@gmail.com>
2012-11-01 20:25:09 -0400

    add photo uploader to artist model + form

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -12,10 +12,13 @@
 #
 
 class Artist < ActiveRecord::Base
-  attr_accessible :bio, :dob, :name, :photo
+  attr_accessible :bio, :dob, :name, :photo, :remote_photo_url
   has_and_belongs_to_many :songs
-  validates :name, :presence => :true
 
   has_many :albums, :through => :songs
+
+  validates :name, :presence => :true
+
+  mount_uploader :photo, PixfileUploader
 
 end
