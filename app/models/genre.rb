1a154221d6d06102aa739c9c8672b25ec54716e2
Lara Iden <lara.iden@gmail.com>
2012-11-05 09:07:04 -0500

    added genre validation, fixed asset tag on home page

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,7 +11,7 @@
 class Genre < ActiveRecord::Base
   attr_accessible :name, :song_ids
 
-  validates :name, :presence => true
+  validates :name, :presence => true, :uniqueness => true
 
   has_and_belongs_to_many :songs
   has_many :albums, :through => :songs
