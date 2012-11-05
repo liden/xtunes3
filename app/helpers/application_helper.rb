0e34cfbb5d5e77155fe402a67cfd33b5142654d6
Lara Iden <lara.iden@gmail.com>
2012-11-04 21:58:53 -0500

    create helper for album cover default -- revisit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 module ApplicationHelper
 
+  def album_cover_file(album)
+    @album.cover_file.present? ? (image_tag(album.cover_file_url.to_s, :size => "150x150", :class => "img-polaroid")) : (image_tag("/assets/albumcover.png", :size => "150x150", :class => "img-polaroid"))
+  end
+
 end
