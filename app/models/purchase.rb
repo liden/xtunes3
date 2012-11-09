1c9f40948ccacc1dc443033e0ba926f3763f9d59
Lara Iden <lara.iden@gmail.com>
2012-11-09 11:26:25 -0500

    add date col to purchases + set up form

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,12 +2,13 @@
 #
 # Table name: purchases
 #
-#  id         :integer          not null, primary key
-#  price_paid :decimal(, )
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :integer
-#  song_id    :integer
+#  id            :integer          not null, primary key
+#  price_paid    :decimal(, )
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  user_id       :integer
+#  song_id       :integer
+#  purchase_date :date
 #
 
 class Purchase < ActiveRecord::Base
