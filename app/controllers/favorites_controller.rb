97c16647e12a1003ff524184ae935f2777068d71
Lara Iden <lara.iden@gmail.com>
2012-11-09 11:25:51 -0500

    add favorite model + controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,16 @@
 class FavoritesController < ApplicationController
 
-  def new
+  def edit
+    favorite = Favorite.find(params[:id])
+    if favorite.is_fave == true
+      favorite.is_fave == false
+      favorite.save
+      redirect_to songs_collection_path, notice: "#{song.name} is no longer one of your favorites."
+    else
+      favorite.is_fave == true
+      favorite.save
+      redirect_to songs_collection_path, notice: "#{song.name} is now one of your favorites."
+    end
   end
 
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,15 +3,15 @@
 # Table name: favorites
 #
 #  id         :integer          not null, primary key
-#  favorite   :boolean          default(FALSE)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  song_id    :integer
 #  user_id    :integer
+#  is_fave    :boolean          default(FALSE)
 #
 
 class Favorite < ActiveRecord::Base
-  attr_accessible :favorite, :user_id, :song_id
+  attr_accessible :is_fave, :user_id, :song_id
   belongs_to :user
   belongs_to :song
 end
